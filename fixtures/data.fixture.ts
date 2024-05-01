985f914ddcf90ac8e0b0812cc4e42098af45d4a2
kristinap8 <kristinapoberezhna8@gmail.com>
2024-05-01 11:17:04 +0200

    Add fixture for loginForm and myProfile data

--- fixtures/data.fixture.ts
+++ fixtures/data.fixture.ts
@@ -1,11 +1,15 @@
 import { test as base } from '@playwright/test';
 import { servicesData } from '../data/sectionsData/servicesData.json';
 import { vehiclesData } from '../data/sectionsData/vehiclesData.json';
+import * as loginFormData from '../data/loginData/loginFormData.json';
+import * as myProfileData from '../data/myProfileData/data.json';
 import * as endpoints from '../data/endpoints.data.json';
 
 export type testData = {
-    servicesData: typeof servicesData,
+    servicesData: typeof servicesData
     vehiclesData: typeof vehiclesData
+    loginFormData: typeof loginFormData
+    myProfileData: typeof myProfileData
     endpointsData: typeof endpoints
 }
 
@@ -15,6 +19,12 @@ const testData = base.extend<testData>({
     },
     vehiclesData: async ({ }, use) => {
         await use(vehiclesData);
+    },
+    loginFormData: async ({ }, use) => {
+        await use(loginFormData);
+    },
+    myProfileData: async ({ }, use) => {
+        await use(myProfileData);
     },
     endpointsData: async({ }, use) => {
         await use(endpoints);
