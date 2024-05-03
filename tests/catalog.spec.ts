1b9926731cca9a0434557c8266327b882b99ca8b
kristinap8 <kristinapoberezhna8@gmail.com>
2024-05-03 23:11:09 +0200

    Refactor test for catalog

--- tests/catalog.spec.ts
+++ tests/catalog.spec.ts
@@ -1,88 +1,75 @@
-// import { test, expect } from '@playwright/test';
-// import { NavBar } from '../pages/components/navbar';
-// import { UnitsPage } from '../pages/units.page';
-// import catalogMenuItems from '../fixtures/catalogMenuItems.json';
-// import catalogMenuVehicles from '../fixtures/catalogMenuVehicles.json';
-// import catalogMenuServices from '../fixtures/catalogMenuServices.json';
+import { test, expect, pages, testData } from "../fixtures/fixture";
 
+async function verifySubmenuItems(navBar: pages["navBar"], level: number, expectedSubMenuItems: testData["menuItems"]) {
+    const submenuItems = await navBar.getCatalogSubmenuItemsArray(level);
+    for (let i = 0; i < submenuItems.length; i++) {
+        await expect(submenuItems[i]).toBeVisible();
+        await expect(submenuItems[i]).toHaveText(expectedSubMenuItems[i].name);
+    }
+}
 
-// let navBar: NavBar;
-// let unitsPage: UnitsPage;
+async function hoverAndVerifyCatalogMenuItems(navBar: pages["navBar"], level: number, menuItemToHover: any) {
+    await navBar.hoverCatalogElementByLevelAndName(level, menuItemToHover['name']);
+    await verifySubmenuItems(navBar, level + 1, menuItemToHover['subMenu']);
+}
 
-// test.describe('Catalog menu check', () => {
-//     test.beforeEach(async ({ page }) => {
-//         navBar = new NavBar(page);
-//         unitsPage = new UnitsPage(page);
+async function hoverAndClick(navBar: pages["navBar"], levelToHover: number, names: string[]) {
+    for (let i = 1; i <= levelToHover; i++) {
+        await navBar.hoverCatalogElementByLevelAndName(i, names[i - 1]);
+    }
+    await navBar.clickCatalogElementByLevelAndName(levelToHover + 1, names[names.length - 1]);
+}
 
-//         await navBar.openUrl();
-//         await expect(await navBar.getLogo()).toBeVisible();
-//     });
+async function returnToMainPage(navBar: pages["navBar"]) {
+    await navBar.clickNavbarItem('logo');
+    await navBar.clickNavbarItem('catalogBtn');
+}
 
-//     test('C559 - Verify "Каталог"', async () => {
-//         await expect(await navBar.getCatalogLabel()).toBeVisible();
+test.describe('Catalog menu check', () => {
+    test.beforeEach(async ({ navBar, telegramPopUp }) => {
+        await telegramPopUp.closeTelegramPopUp();
+        await navBar.openUrl();
+        await expect(navBar.getNavbarItem('logo')).toBeVisible();
+    });
 
-//         for (let i = 0; i < catalogMenuVehicles.subMenu.length; i++) {
-//             await navBar.clickCatalog();
-//             await expect(await navBar.getCatalogDropdownMenu()).toBeVisible();
+    test('C559 - Verify "Каталог"', async ({ navBar, unitsPage, menuItems, endpointsData }) => {
+        await expect(navBar.getNavbarItem('catalogBtn')).toBeVisible();
+        await navBar.clickNavbarItem('catalogBtn');
+        await expect(navBar.getCatalogDropdown()).toBeVisible();
 
-//             const firstLvlSubMenuItems = await navBar.getFirstLvlCatalogSubMenuItems();
-//             for (let j = 0; j < firstLvlSubMenuItems.length; j++) {
-//                 await expect(firstLvlSubMenuItems[j]).toBeVisible();
-//                 await expect(firstLvlSubMenuItems[j]).toHaveText(catalogMenuItems.menuItems[j]);
-//             }
+        //verify first level catalog menu items
+        await verifySubmenuItems(navBar, 1, menuItems);
+        for (let i = 0; i < menuItems.length; i++) {
+            //verify second level catalog menu items
+            await hoverAndVerifyCatalogMenuItems(navBar, 1, menuItems[i]);
+            for (let j = 0; j < menuItems[i].subMenu.length; j++) {
+                //verify third level catalog menu items
+                await hoverAndVerifyCatalogMenuItems(navBar, 2, menuItems[i].subMenu[j]);
+            }
+        }
 
-//             await navBar.hoverCatalogElementByLevelAndInd(1, 0);
-//             const secondLvlSubMenuItems = await navBar.getSecondLvlCatalogSubMenuItems();
-//             for (let j = 0; j < secondLvlSubMenuItems.length; j++) {
-//                 await expect(secondLvlSubMenuItems[j]).toBeVisible();
-//                 await expect(secondLvlSubMenuItems[j]).toHaveText(catalogMenuVehicles.subMenu[j].name);
-//             }
+        // Verify URL and selected filter for vehicles second level submenu items
+        const vehiclesMenuItem = menuItems[0].name;
+        const vehiclesSndLvlMenuItems = menuItems[0].subMenu;
+        for (let j = 0; j < vehiclesSndLvlMenuItems.length; j++) {
+            await hoverAndClick(navBar, 1, [vehiclesMenuItem, vehiclesSndLvlMenuItems[j].name]);
+            await expect(unitsPage.page).toHaveURL(vehiclesSndLvlMenuItems[j]['url']);
+            await expect(await unitsPage.getSelectedFilter()).toHaveText(vehiclesSndLvlMenuItems[j].name);
+            await returnToMainPage(navBar);
+        }
 
-//             await navBar.clickCatalogElementByLevelAndInd(2, i);
-//             await expect(productsPage.page).toHaveURL(catalogMenuVehicles.subMenu[i].url);
-//             await expect(await productsPage.getSelectedFilter()).toHaveText(catalogMenuVehicles.subMenu[i].name);
-
-//             await navBar.clickLogo();
-
-//             for (let j = 0; j < catalogMenuVehicles.subMenu[i].subMenu.length; j++) {
-//                 await navBar.clickCatalog();
-//                 await navBar.hoverCatalogElementByLevelAndInd(1, 0);
-//                 await navBar.hoverCatalogElementByLevelAndInd(2, i);
-
-//                 const thirdLvlSubMenuItems = await navBar.getThirdLvlCatalogSubMenuItems();
-//                 for (let k = 0; k < thirdLvlSubMenuItems.length; k++) {
-//                     await expect(thirdLvlSubMenuItems[k]).toBeVisible();
-//                     await expect(thirdLvlSubMenuItems[k]).toHaveText(catalogMenuVehicles.subMenu[i].subMenu[k].name);
-//                 }
-//                 await navBar.clickCatalogElementByLevelAndInd(3, j);
-//                 await expect(productsPage.page).toHaveURL(catalogMenuVehicles.subMenu[i].subMenu[j].url);
-//                 await navBar.clickLogo();
-//             }
-//         }
-
-//         for (let i = 0; i < catalogMenuServices.subMenu.length; i++) {
-//             await navBar.clickCatalog();
-//             await navBar.hoverCatalogElementByLevelAndInd(1, 1);
-
-//             const secondLvlSubMenuItems = await navBar.getSecondLvlCatalogSubMenuItems();
-//             for (let j = 0; j < secondLvlSubMenuItems.length; j++) {
-//                 await expect(secondLvlSubMenuItems[j]).toBeVisible();
-//                 await expect(secondLvlSubMenuItems[j]).toHaveText(catalogMenuServices.subMenu[j].name);
-//             }
-
-//             await navBar.hoverCatalogElementByLevelAndInd(2, i);
-//             const thirdLvlSubMenuItems = await navBar.getThirdLvlCatalogSubMenuItems();
-//             for (let j = 0; j < thirdLvlSubMenuItems.length; j++) {
-//                 await expect(thirdLvlSubMenuItems[j]).toBeVisible();
-//                 await expect(thirdLvlSubMenuItems[j]).toHaveText(catalogMenuServices.subMenu[i].subMenu[j]);
-//             }
-
-//             const labelInd = catalogMenuServices.subMenu[i].subMenu.indexOf(catalogMenuServices.subMenu[i].searchLabel);
-//             await navBar.clickCatalogElementByLevelAndInd(3, labelInd);
-//             await expect(productsPage.page).toHaveURL(/\/products\/$/);
-//             await expect(await productsPage.getSelectedFilter()).toHaveText(catalogMenuServices.subMenu[i].searchLabel, { ignoreCase: true });
-
-//             await navBar.clickLogo();
-//         }
-//     })
-// })
+        // Verify URL and selected filter for vehicles and services third level submenu items
+        for (let i = 0; i < menuItems.length; i++) {
+            const sndLvlMenuItems = menuItems[i].subMenu;
+            for (let j = 0; j < sndLvlMenuItems.length; j++) {
+                const thirdLvlMenuItems = sndLvlMenuItems[j].subMenu;
+                for (let k = 0; k < thirdLvlMenuItems.length; k++) {
+                    await hoverAndClick(navBar, 2, [menuItems[i].name, sndLvlMenuItems[j].name, thirdLvlMenuItems[k].name]);
+                    (i === 0) ? await expect(unitsPage.page).toHaveURL(thirdLvlMenuItems[k]['url']) : await expect(unitsPage.page).toHaveURL(endpointsData["units"]);
+                    await expect(await unitsPage.getSelectedFilter()).toHaveText(thirdLvlMenuItems[k].name);
+                    await returnToMainPage(navBar);
+                }
+            }
+        }
+    })
+})
